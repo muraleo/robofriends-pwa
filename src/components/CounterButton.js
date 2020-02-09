6a841000357c3de54c76110c79022cfc9edea7d2
muraleo <muraleo80923@gmail.com>
2020-02-08 23:56:27 -0500

    add simulate function

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class CounterButton extends Component {
   constructor(props) {
     super(props);
-    this.state = {count: 1};
+    this.state = { count: 1 };
   }
 
   shouldComponentUpdate(nextProps, nextState) {
@@ -17,8 +17,9 @@ class CounterButton extends Component {
     console.log('counter');
     return (
       <button
+        id="counter"
         color={this.props.color}
-        onClick={() => this.setState(state => ({count: state.count + 1}))}>
+        onClick={() => this.setState(state => ({ count: state.count + 1 }))}>
         Count: {this.state.count}
       </button>
     );
--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -4,6 +4,10 @@ import CounterButton from './CounterButton';
 
 
 it('test counter button', () => {
+    expect.assertions(2);
     const wrapper = shallow(<CounterButton />);
     expect(wrapper.state('count')).toEqual(1);
+
+    wrapper.find('button').simulate('click');
+    expect(wrapper.state('count')).toEqual(2);
 })
